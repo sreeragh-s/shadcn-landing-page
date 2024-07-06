a17c584605f56ac202e5aa74da3bd57bd6a485cd
sreeragh-s <sreeragh@bizmo.site>
2024-07-06 17:01:14 +0530

    Update features.tsx

--- components/layout/sections/features.tsx
+++ components/layout/sections/features.tsx
@@ -76,7 +76,7 @@ export const FeaturesSection = () => {
                   />
                 </div>
 
-                <CardTitle>{title}</CardTitle>
+                <CardTitle className="text-center">{title}</CardTitle>
               </CardHeader>
 
               <CardContent className="text-muted-foreground text-center">
